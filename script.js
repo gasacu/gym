e417a79add4cc7e96665f2f2c3508d2362cea5f7
GabrielSacuianu <gsacuianu@gmail.com>
2024-04-14 18:55:10 +0300

    mobile dropdown menu

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+//MOBILE MENU TRIGGER BUTTON//
+let $navTrigger = document.getElementById('navTrigger');
+let $navMenu = document.getElementById('navMenu');
+
+$navTrigger.addEventListener('click' , () => {
+    $navMenu.classList.toggle('Active');
+}, true);
+
+
+
 // Slider //
 var slideIndex = 1;
 showSlides(slideIndex);
@@ -47,50 +57,3 @@ function showSlides() {
 }
 */
 
-// Accordion Menu List //
-const Accordion = document.getElementsByClassName('ContentBx');
-
-for(i = 0; i<Accordion.length; i++) {
-    Accordion[i].addEventListener('click', function(){
-        this.classList.toggle('Active1')
-    })
-}
-
-//MOBILE MENU TRIGGER BUTTON//
-let $navTrigger = document.getElementById('navTrigger');
-let $navMenu = document.getElementById('navMenu');
-
-$navTrigger.addEventListener('click' , () => {
-    $navMenu.classList.toggle('Active');
-}, true);
-
-// Desktop Second Menu Active //
-
-document.querySelectorAll('.MainMenuLink').forEach(item => {
-    item.addEventListener('click', function() {
-        // Toogle active class to show or hide the sublist
-        var sublist = this.nextElementSibling;
-        if (sublist.classList.contains('Active')) {
-            sublist.classList.remove('Active');
-        } else {
-            // Hide all other sublists
-            document.querySelectorAll('.ContentBx').forEach(sub => {
-                sub.classList.remove('Active');
-            });
-            // Show this sublist
-            sublist.classList.add('Active');
-        }
-    });
-});
-
-document.addEventListener('click', function(event) {
-    var navMenu = document.querySelector('.MainMenu');
-    var clickedInsideNavMenu = navMenu.contains(event.target);
-
-    if (!clickedInsideNavMenu) {
-        //Clicked outside .MainMenu, so hide all sublists
-        document.querySelectorAll('.ContentBx').forEach(sub => {
-            sub.classList.remove('Active');
-        });
-    }
-});
